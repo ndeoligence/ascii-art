931f78d9bb5258f4a263cd8882e9e07e501f533c
Monde <monde.ngalo@gmail.com>
2020-10-02 00:52:58 +0200

    Code reformat

--- community-version.py
+++ community-version.py
@@ -1,22 +1,26 @@
 # this project requires Pillow installation: https://pillow.readthedocs.io/en/stable/installation.html
 
-#code credit goes to: https://www.hackerearth.com/practice/notes/beautiful-python-a-simple-ascii-art-generator-from-images/
-#code modified to work with Python 3 by @aneagoie
+# code credit goes to: https://www.hackerearth.com/practice/notes/beautiful-python-a-simple-ascii-art-generator-from-images/
+# code modified to work with Python 3 by @aneagoie
 from PIL import Image
-ASCII_CHARS = [ '#', '?', '%', '.', 'S', '+', '.', '*', ':', ',', '@']
+
+ASCII_CHARS = ['#', '?', '%', '.', 'S', '+', '.', '*', ':', ',', '@']
+
 
 def scale_image(image, new_width=100):
     """Resizes an image preserving the aspect ratio.
     """
     (original_width, original_height) = image.size
-    aspect_ratio = original_height/float(original_width)
+    aspect_ratio = original_height / float(original_width)
     new_height = int(aspect_ratio * new_width)
 
     new_image = image.resize((new_width, new_height))
     return new_image
 
+
 def convert_to_grayscale(image):
     return image.convert('L')
+
 
 def map_pixels_to_ascii_chars(image, range_width=25):
     """Maps each pixel to an ascii char based on the range
@@ -26,10 +30,11 @@ def map_pixels_to_ascii_chars(image, range_width=25):
     """
 
     pixels_in_image = list(image.getdata())
-    pixels_to_chars = [ASCII_CHARS[int(pixel_value/range_width)] for pixel_value in
-            pixels_in_image]
+    pixels_to_chars = [ASCII_CHARS[int(pixel_value / range_width)] for pixel_value in
+                       pixels_in_image]
 
     return "".join(pixels_to_chars)
+
 
 def convert_image_to_ascii(image, new_width=100):
     image = scale_image(image)
@@ -39,17 +44,17 @@ def convert_image_to_ascii(image, new_width=100):
     len_pixels_to_chars = len(pixels_to_chars)
 
     image_ascii = [pixels_to_chars[index: index + new_width] for index in
-            range(0, len_pixels_to_chars, new_width)]
+                   range(0, len_pixels_to_chars, new_width)]
 
     return "\n".join(image_ascii)
 
-def write_image_to_text_file(image_ascii):
 
+def write_image_to_text_file(image_ascii):
     with open("ascii_image.txt", "w") as f:
         f.write(image_ascii)
 
+
 def handle_image_conversion(image_filepath, save):
-    image = None
     try:
         image = Image.open(image_filepath)
     except Exception as e:
@@ -62,7 +67,8 @@ def handle_image_conversion(image_filepath, save):
     if save == 1:
         write_image_to_text_file(image_ascii)
 
-if __name__=='__main__':
+
+if __name__ == '__main__':
     import sys
 
     image_file_path = sys.argv[1]
